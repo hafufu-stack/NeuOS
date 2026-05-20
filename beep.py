db4f12c577a3ae38b2d75b44dab54d4d67698922
hafufu-stack <kyjanainaware@gmail.com>
2026-05-20 12:59:00 +0900

    Remove utility scripts (beep, runner) not part of core repo

--- beep.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Beep notification script. Spawned in a new window by runner.py."""
-import winsound, time
-print("=== NeuOS: ALL EXPERIMENTS COMPLETE! ===")
-for _ in range(5):
-    winsound.Beep(1000, 500)
-    time.sleep(0.3)
-print("Press Enter to close...")
-input()
